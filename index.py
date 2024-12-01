6cca527ff1fdda9b79c7dd00f96eed7b8c1e45ce
quangnd <quangnd@coccoc.com>
2024-12-01 19:27:13 +0700

    initial push

---
+++ index.py
@@ -0,0 +1,11 @@
+import customtkinter
+
+customtkinter.set_appearance_mode('system')
+
+root = customtkinter.CTk()
+root.title('Nguyen Duy Quang')
+root.geometry("500x300")
+root.resizable(width=False, height=False)
+
+
+root.mainloop()
